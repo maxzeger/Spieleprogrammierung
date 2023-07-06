6a26bbe422d0c59efbf01481bc71ba0714884974
Can <92165802+Can557@users.noreply.github.com>
2023-07-06 17:27:44 +0200

    Added Blacksmiths

---
+++ Assets/Scripts/Village/Blacksmiths/Defense.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defense : MonoBehaviour
+{
+
+    private bool playerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && playerInRange)
+        {
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
